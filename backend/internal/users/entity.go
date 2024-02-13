7f1a7f286aa39d8ab6ac09a33d88370ff72d5204
thepokenik <nikolaspedro.np@gmail.com>
2024-02-13 19:19:15 -0300

    Add User entity

---
+++ backend/internal/users/entity.go
@@ -0,0 +1,11 @@
+package users
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID         uuid.UUID `json:"id"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	Password   string    `json:"password"`
+	ProfilePic []byte    `json:"profile_pic"`
+}
