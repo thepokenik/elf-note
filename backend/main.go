3d2d6865d4a0bbc00aeb36a6bf5f34f2677eaa19
thepokenik <nikolaspedro.np@gmail.com>
2024-02-13 01:58:08 -0300

    Add CORS middleware and API endpoints for user registration, update, and retrieval

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type User struct {
@@ -17,11 +18,85 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	users := []User{}
 
-	app.Get("/test", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Post("/api/register", func(c *fiber.Ctx) error {
+		var user User
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": true,
+				"message": "Invalid JSON",
+			})
+		}
+		users = append(users, user)
+		return c.JSON(fiber.Map{
+			"message": "User created",
+			"user": user,
+		})
+	})
+
+	app.Patch("/api/user/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": true,
+				"message": "Invalid ID",
+			})
+		}
+
+		for i, user := range users {
+			if user.ID == id {
+				var newUser User
+				if err := c.BodyParser(&newUser); err != nil {
+					return c.Status(400).JSON(fiber.Map{
+						"error": true,
+						"message": "Invalid JSON",
+					})
+				}
+				users[i] = newUser
+				return c.JSON(fiber.Map{
+					"message": "User updated",
+					"user": newUser,
+				})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{
+			"error": true,
+			"message": "User not found",
+		})
+	})
+
+	app.Get("/api/user/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": true,
+				"message": "Invalid ID",
+			})
+		}
+
+		for _, user := range users {
+			if user.ID == id {
+				return c.JSON(fiber.Map{
+					"user": user,
+				})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{
+			"error": true,
+			"message": "User not found",
+		})
 	})
 
 	log.Fatal(app.Listen(":4000"))
+
 }
