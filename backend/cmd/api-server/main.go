24c12d82c563ef6b62a94dd91cd3fa8f3d6de592
thepokenik <nikolaspedro.np@gmail.com>
2024-02-17 01:45:00 -0300

    Update AllowOrigins to http://localhost:5173/Electron

--- backend/cmd/api-server/main.go
+++ backend/cmd/api-server/main.go
@@ -22,7 +22,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "http://localhost:5173",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
