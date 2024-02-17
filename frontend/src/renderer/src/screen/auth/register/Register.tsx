4090c6799a74b8127d2b23495e4412244421ddb4
thepokenik <nikolaspedro.np@gmail.com>
2024-02-17 17:14:15 -0300

    Remove unused ProfileImage type definition

--- frontend/src/renderer/src/screen/auth/register/Register.tsx
+++ frontend/src/renderer/src/screen/auth/register/Register.tsx
@@ -3,8 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import ScrollReveal from 'scrollreveal';
 import userBaseImage from '../assets/user.svg';
 
-type ProfileImage = File | null;
-
 interface User {
     id: null;
     username: string;
